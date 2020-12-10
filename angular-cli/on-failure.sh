92175480a7440533aaa8b60f7b7982a2c5e91663
PedroHLC <root@pedrohlc.com>
2020-12-10 20:35:19 -0300

    Last changes from lonewolf cluster

---
+++ angular-cli/on-failure.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+[ -f ../firefox-wayland-hg.log ] && \
+telegram-send \
+	--config ~/.config/telegram-send-group.conf \
+	--silent "Ohayo @pedrohlc-senpai \"anguwaw-cwi\" has a nyew update UwU  Do that manyuaw thing you awways have to do..." 
