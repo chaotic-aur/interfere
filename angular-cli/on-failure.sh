71c2f5a016e9e75aad44d169250196c4dd8cf897
dr460nf1r3 <njcrypted@protonmail.com>
2021-02-08 14:46:09 +0100

    Clean up obsolete

--- angular-cli/on-failure.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-[ -f ../firefox-wayland-hg.log ] && \
-telegram-send \
-	--config ~/.config/telegram-send-group.conf \
-	--silent "Ohayo @pedrohlc-senpai \"anguwaw-cwi\" has a nyew update UwU  Do that manyuaw thing you awways have to do..." 
