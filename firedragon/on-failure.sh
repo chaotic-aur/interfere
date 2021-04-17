41f8c61e1b45804867990080eca2fbbbc1733eb0
dr460nf1r3 <njcrypted@protonmail.com>
2021-04-17 20:32:59 +0200

    Remove firedragon on failure

--- firedragon/on-failure.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-[ -f ../firedragon.log ] && \
-telegram-send \
-	--config ~/.config/telegram-send-group.conf \
-	--sile "Hey @dr460nf1r3 \"firedragon\" build failed, logs attached 🐉" \
-	-f ../firedragon.log
