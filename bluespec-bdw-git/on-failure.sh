54e9ca6b79fb39ab6ac05feabdd410b7991a5709
Paulo Matias <matias@ufscar.br>
2021-11-07 14:03:07 -0300

    notify mantainer on failure for bluespec{,-bdw}-git

---
+++ bluespec-bdw-git/on-failure.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+[ -f ../bluespec-bdw-git.log ] && \
+send-log \
+	--sile "Ohayo @thotypous-senpai \"bluespec-bdw-git\" buiwd faiwed, hewe's the wog:" \
+	-f ../bluespec-bdw-git.log
---
+++ bluespec-git/on-failure.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+[ -f ../bluespec-git.log ] && \
+send-log \
+	--sile "Ohayo @thotypous-senpai \"bluespec-git\" buiwd faiwed, hewe's the wog:" \
+	-f ../bluespec-git.log
