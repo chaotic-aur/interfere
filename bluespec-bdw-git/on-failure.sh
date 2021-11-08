b3bf81377ce815da29928878a93e860bff131425
Paulo Matias <matias@ufscar.br>
2021-11-07 23:58:43 -0300

    ignore failure if already been built

--- bluespec-bdw-git/on-failure.sh
+++ bluespec-bdw-git/on-failure.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
-[ -f ../bluespec-bdw-git.log ] && \
-send-log \
-	--sile "Ohayo @thotypous-senpai \"bluespec-bdw-git\" buiwd faiwed, hewe's the wog:" \
-	-f ../bluespec-bdw-git.log
+if [ -f ../bluespec-bdw-git.log ]; then
+    if ! grep -F 'ERROR: A package has already been built. (use -f to overwrite)' ../bluespec-bdw-git.log >/dev/null; then
+        send-log \
+            --sile "Ohayo @thotypous-senpai \"bluespec-bdw-git\" buiwd faiwed, hewe's the wog:" \
+            -f ../bluespec-bdw-git.log
+    fi
+fi
--- bluespec-git/on-failure.sh
+++ bluespec-git/on-failure.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
-[ -f ../bluespec-git.log ] && \
-send-log \
-	--sile "Ohayo @thotypous-senpai \"bluespec-git\" buiwd faiwed, hewe's the wog:" \
-	-f ../bluespec-git.log
+if [ -f ../bluespec-git.log ]; then
+    if ! grep -F 'ERROR: A package has already been built. (use -f to overwrite)' ../bluespec-git.log >/dev/null; then
+        send-log \
+            --sile "Ohayo @thotypous-senpai \"bluespec-git\" buiwd faiwed, hewe's the wog:" \
+            -f ../bluespec-git.log
+    fi
+fi
