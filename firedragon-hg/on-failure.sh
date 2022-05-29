6af8b55363722847c11812f3d6f93723d12b7b35
dr460nf1r3 <njcrypted@protonmail.com>
2022-05-29 10:19:12 +0200

    Drop a bunch of redundant interferes

--- firedragon-hg/on-failure.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-[ -f ../firedragon-hg.log ] && \
-send-log \
-	--sile "Hey @dr460nf1r3 \"firedragon-hg\" build failed, logs attached 🐉" \
-	-f ../firedragon-hg.log
--- librewolf-hg/on-failure.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-[ -f ../librewolf-hg.log ] && \
-send-log \
-	--sile "Hey @vnepogodin \"librewolf-hg\" build failed, logs are attached. :)" \
-	-f ../librewolf-hg.log
