edd30dc3bd7ebebbf6f334933b7026ad21ffbccd
xiota <xiota@users.noreply.github.com>
2023-12-31 05:56:31 -0800

    firefox-wayland-hg: examine symbol archive

--- firefox-wayland-hg/on-success.sh
+++ firefox-wayland-hg/on-success.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash -xe
+printf "Symbol archive: %s\n" pkgwork/*crashreporter-symbols-full.tar.zst || true
+file -Ebi ./pkgwork/*crashreporter-symbols-full.tar.zst || true
+stat -s ./pkgwork/*crashreporter-symbols-full.tar.zst || true
+tar tf ./pkgwork/*crashreporter-symbols-full.tar.zst || true
+
 ./pkgwork/upload-symbol-archive "$HOME/.config/token-mozilla-symbols" ./pkgwork/*crashreporter-symbols-full.tar.zst
