35d98b4acba524b99ee1780ad73823011f5f6417
xiota <xiota@users.noreply.github.com>
2024-01-03 12:51:44 -0800

    firefox-wayland-hg: fix symbol archive mimetype

--- firefox-wayland-hg/on-success.sh
+++ firefox-wayland-hg/on-success.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env bash -xe
+#!/usr/bin/env sh
+
+# symbol archive info
 printf "Symbol archive: %s\n" pkgwork/*crashreporter-symbols-full.tar.zst || true
 file -Ebi ./pkgwork/*crashreporter-symbols-full.tar.zst || true
-stat -s ./pkgwork/*crashreporter-symbols-full.tar.zst || true
-tar tf ./pkgwork/*crashreporter-symbols-full.tar.zst || true
 
+# fix mimetype
+sed -E 's&application/zstd&application/x-zstd&' -i ./pkgwork/upload-symbol-archive
+
+# upload symbols
 ./pkgwork/upload-symbol-archive "$HOME/.config/token-mozilla-symbols" ./pkgwork/*crashreporter-symbols-full.tar.zst
