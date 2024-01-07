1a126176665a4ac3af8a6f2d6b58ef860e14a0d8
xiota <xiota@users.noreply.github.com>
2024-01-07 03:46:56 -0800

    firefox-wayland-hg: use zip symbol archive

--- firefox-wayland-hg/on-success.sh
+++ firefox-wayland-hg/on-success.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 # symbol archive info
-printf "Symbol archive: %s\n" pkgwork/*crashreporter-symbols-full.tar.zst || true
-file -Ebi ./pkgwork/*crashreporter-symbols-full.tar.zst || true
+printf "Symbol archive: %s\n" pkgwork/*crashreporter-symbols.zip || true
+file -Ebi ./pkgwork/*crashreporter-symbols.zip || true
 
 # upload symbols
-./pkgwork/upload-symbol-archive "$HOME/.config/token-mozilla-symbols" ./pkgwork/*crashreporter-symbols-full.tar.zst
+./pkgwork/upload-symbol-archive "$HOME/.config/token-mozilla-symbols" ./pkgwork/*crashreporter-symbols.zip
